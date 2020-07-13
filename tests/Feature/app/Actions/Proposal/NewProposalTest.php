f48f3871c7fe603b5cc1fff1429f3eaf32698e7c
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-07-13 01:03:06 +0100

    add test for new proposal redirect if multiple org

--- tests/Feature/app/Actions/Proposal/NewProposalTest.php
+++ tests/Feature/app/Actions/Proposal/NewProposalTest.php
@@ -24,3 +24,14 @@ it('creates proposal if user is member of one org, then redirects to view propos
     $proposal = $org->proposals->first();
     $response->assertRedirect(route('use.proposal.view', ['proposal' => $proposal->uuid]));
 });
+
+it('redirects to choose organisation page, if user is a member of multiple orgs', function () {
+    $proposalCountBefore = Proposal::count();
+    $user = factory(User::class)->create();
+    $org1 = $user->organisations()->create(['name' => 'org1']);
+    $org2 = $user->organisations()->create(['name' => 'org2']);
+
+    $response = actingAs($user)->get('/proposals/new');
+    $response->assertRedirect(route('use.proposal.new.choose-org'));
+    assertEquals($proposalCountBefore, Proposal::count());
+});
