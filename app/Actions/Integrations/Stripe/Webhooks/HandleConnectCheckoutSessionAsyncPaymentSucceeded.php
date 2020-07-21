6f5db25c0b572c9d6b6bb88119e8f330617a5a9a
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-07-22 00:49:05 +0100

    stripe webhook handling wip boilerplate #19

---
+++ app/Actions/Integrations/Stripe/Webhooks/HandleConnectCheckoutSessionAsyncPaymentSucceeded.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Actions\Integrations\Stripe\Webhooks;
+
+use CloudCreativity\LaravelStripe\Webhooks\ConnectWebhook;
+use Lorisleiva\Actions\Action;
+
+class HandleConnectCheckoutSessionAsyncPaymentSucceeded extends Action
+{
+    public function getAttributesFromEvent(ConnectWebhook $event)
+    {
+        return [
+            'webhook' => $event,
+        ];
+    }
+
+    /**
+     * Execute the action and return a result.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Execute the action.
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use App\Actions\Integrations\Stripe\HandleStripeConnectFetchedUserCredentials;
+use App\Actions\Integrations\Stripe\Webhooks\HandleConnectCheckoutSessionAsyncPaymentFailed;
+use App\Actions\Integrations\Stripe\Webhooks\HandleConnectCheckoutSessionAsyncPaymentSucceeded;
+use App\Actions\Integrations\Stripe\Webhooks\HandleConnectCheckoutSessionCompleted;
+use App\Actions\Integrations\Stripe\Webhooks\HandleConnectPaymentIntentPaymentFailed;
+use App\Actions\Integrations\Stripe\Webhooks\HandleConnectPaymentIntentProcessing;
+use App\Actions\Integrations\Stripe\Webhooks\HandleConnectPaymentIntentSucceeded;
 use CloudCreativity\LaravelStripe\Events\FetchedUserCredentials;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -31,7 +37,50 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        Event::listen(FetchedUserCredentials::class, HandleStripeConnectFetchedUserCredentials::class);
+        $this->handleStripeConnectEvents();
+    }
+
+    /**
+     * Handle events from Stripe Connect integration.
+     *
+     * @return void
+     */
+    protected function handleStripeConnectEvents()
+    {
+        Event::listen(
+            FetchedUserCredentials::class,
+            HandleStripeConnectFetchedUserCredentials::class
+        );
+
+        Event::listen(
+            'stripe.connect.webhooks:payment_intent.processing',
+            HandleConnectPaymentIntentProcessing::class
+        );
+
+        Event::listen(
+            'stripe.connect.webhooks:payment_intent.succeeded',
+            HandleConnectPaymentIntentSucceeded::class
+        );
+
+        Event::listen(
+            'stripe.connect.webhooks:payment_intent.payment_failed',
+            HandleConnectPaymentIntentPaymentFailed::class
+        );
+
+        Event::listen(
+            'stripe.connect.webhooks:checkout.session.completed',
+            HandleConnectCheckoutSessionCompleted::class
+        );
+
+        Event::listen(
+            'stripe.connect.webhooks:checkout.session.async_payment_succeeded',
+            HandleConnectCheckoutSessionAsyncPaymentSucceeded::class
+        );
+
+        Event::listen(
+            'stripe.connect.webhooks:checkout.session.async_payment_failed',
+            HandleConnectCheckoutSessionAsyncPaymentFailed::class
+        );
 
     }
 }
