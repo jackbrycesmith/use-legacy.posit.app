cc905ba17908ff014647835cd0ea65e4e21bc0be
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-08-02 16:04:40 +0100

    WIP: add dashboard layout with user dropdown #59

---
+++ resources/js/Components/UserAccountDropdown.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="relative">
+    <div>
+      <button ref="menuTriggerButton" @click="handleAvatarClick" class="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:bg-orange-100 lg:p-2 lg:rounded-md lg:hover:bg-orange-100" aria-label="User menu" aria-haspopup="true">
+        <div class="h-8 w-8 rounded-full bg-orange-200 text-gray-900 flex items-center justify-center">
+          {{ userAvatarInitial }}
+        </div>
+        <p class="hidden ml-3 text-cool-gray-700 text-sm leading-5 font-medium lg:block">
+          {{ userEmail }}
+        </p>
+        <svg class="hidden flex-shrink-0 ml-1 h-5 w-5 text-cool-gray-400 lg:block" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+      </button>
+    </div>
+    <!--
+      Profile dropdown panel, show/hide based on dropdown state.
+
+      Entering: "transition ease-out duration-100"
+        From: "transform opacity-0 scale-95"
+        To: "transform opacity-100 scale-100"
+      Leaving: "transition ease-in duration-75"
+        From: "transform opacity-100 scale-100"
+        To: "transform opacity-0 scale-95"
+    -->
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95">
+      <div
+        v-click-outside="vcoConfig"
+        v-if="isOpen"
+        class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg">
+        <div class="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+          <a href="#" class="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150" role="menuitem">User Settings</a>
+          <a href="#" class="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150" role="menuitem">Logout</a>
+        </div>
+      </div>
+    </transition>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isOpen: false,
+      vcoConfig: {
+        handler: this.onClickOutside,
+        middleware: this.determineWhetherClickOutside,
+        events: ['dblclick', 'click'],
+        // Note: The default value is true, but in case you want to activate / deactivate
+        //       this directive dynamically use this attribute.
+        isActive: true
+      }
+    }
+  },
+  computed: {
+    userEmail () {
+      // TODO this should ideally be part of some global thing that I can import...
+      return this.$page?.user?.email
+    },
+    userAvatarInitial () {
+      return this.userEmail?.charAt(0)?.toUpperCase()
+    }
+  },
+  methods: {
+    handleAvatarClick () {
+      this.isOpen = !this.isOpen
+    },
+    onClickOutside (event) {
+      if (this.isOpen) {
+        this.isOpen = false
+      }
+    },
+    determineWhetherClickOutside (event) {
+      return event.target.parentElement !== this.$refs.menuTriggerButton
+    }
+  }
+}
+</script>
