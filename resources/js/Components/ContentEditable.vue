9fd4ee8ffdb76bb065ac5dc05fca2edf12a540c2
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-08-09 23:26:52 +0100

    add content editable component

---
+++ resources/js/Components/ContentEditable.vue
@@ -0,0 +1,54 @@
+<template>
+  <!-- TODO prop & logic to strip tags / disable pasting html into here... -->
+  <!-- Actually using v-text may protect me from this... -->
+  <component
+    ref="contentEditable"
+    :is="tag"
+    :contenteditable="isContentEditable"
+    @blur="onEdit"
+    @input="onLiveEdit"
+    @keydown.enter="endEdit"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    tag: { type: String, required: true },
+    isContentEditable: { type: Boolean, default: true },
+    model: { type: String },
+    liveUpdateModel: { type: Boolean, default: true }
+  },
+  data () {
+    return {
+      initialModel: ''
+    }
+  },
+  watch: {
+    model: {
+      immediate: true,
+      handler (value) {
+        this.$nextTick(() => {
+          if (this.$refs.contentEditable.innerText !== value) {
+            this.$refs.contentEditable.innerText = value
+          }
+        })
+      }
+    }
+  },
+  methods: {
+    onEdit (evt) {
+      this.$emit('update:model', evt.target.innerText)
+      this.$emit('edit-done', evt.target.innerText)
+    },
+    onLiveEdit (evt) {
+      if (!this.liveUpdateModel) return
+
+      this.$emit('update:model', evt.target.innerText)
+    },
+    endEdit () {
+      this.$refs.contentEditable.blur()
+    },
+  }
+}
+</script>
