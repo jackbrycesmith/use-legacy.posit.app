516baa3370c9df08b255a9c7210be1184822247e
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-09-11 22:39:25 +0100

    #51 proposal video test

--- tests/Feature/app/Actions/UsePositApp/Submit/ProposalVideoIntroUpsertTest.php
+++ tests/Feature/app/Actions/UsePositApp/Submit/ProposalVideoIntroUpsertTest.php
@@ -1,23 +1,30 @@
 <?php
 
 use App\Actions\Organisation\CreateDraftProposal;
+use App\Jobs\ConvertVideoForDownloading;
 use App\Models\User;
+use App\Models\Video;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use function Tests\actingAs;
 
 test('ProposalVideoIntroUpsert 404s with proposal invalid uuid', function () {
+    Bus::fake();
     $response = $this->post(route('use.proposal.video-intro', 'blah'));
+    Bus::assertNotDispatched(ConvertVideoForDownloading::class);
 
     $response->assertStatus(404);
-});
+})->only();
 
 test('ProposalVideoIntroUpsert requires login', function () {
+    Bus::fake();
     $response = $this->post(route('use.proposal.video-intro', Str::uuid()));
+    Bus::assertNotDispatched(ConvertVideoForDownloading::class);
 
     $response->assertRedirect(route('login'));
-});
+})->only();
 
 test('ProposalVideoIntroUpsert disallowed if not a proposal user', function () {
     $user = factory(User::class)->create();
@@ -27,14 +34,16 @@ test('ProposalVideoIntroUpsert disallowed if not a proposal user', function () {
         'organisation' => $user->organisations->first()
     ]);
 
+    Bus::fake();
     $response = actingAs($otherUser)->post(
         route('use.proposal.video-intro', [
             'proposal' => $proposal
         ])
     );
+    Bus::assertNotDispatched(ConvertVideoForDownloading::class);
 
     $response->assertStatus(403);
-});
+})->only();
 
 test('ProposalVideoIntroUpsert disallowed if incorrect data passed', function () {
     $user = factory(User::class)->create();
@@ -43,6 +52,7 @@ test('ProposalVideoIntroUpsert disallowed if incorrect data passed', function ()
         'organisation' => $user->organisations->first()
     ]);
 
+    Bus::fake();
     $response = actingAs($user)->postJson(
         route('use.proposal.video-intro', [
             'proposal' => $proposal
@@ -50,7 +60,9 @@ test('ProposalVideoIntroUpsert disallowed if incorrect data passed', function ()
     );
     $response->assertStatus(422);
     $response->assertJsonValidationErrors(['uuid' => 'The uuid field is required.']);
+    Bus::assertNotDispatched(ConvertVideoForDownloading::class);
 
+    Bus::fake();
     $response = actingAs($user)->postJson(
         route('use.proposal.video-intro', [
             'proposal' => $proposal
@@ -60,7 +72,8 @@ test('ProposalVideoIntroUpsert disallowed if incorrect data passed', function ()
     );
     $response->assertStatus(422);
     $response->assertJsonValidationErrors(['uuid' => 'The uuid must be a valid UUID.']);
-});
+    Bus::assertNotDispatched(ConvertVideoForDownloading::class);
+})->only();
 
 test('ProposalVideoIntroUpsert disallowed if temp file does not exist', function () {
     Storage::fake('s3');
@@ -71,6 +84,7 @@ test('ProposalVideoIntroUpsert disallowed if temp file does not exist', function
         'organisation' => $user->organisations->first()
     ]);
 
+    Bus::fake();
     $response = actingAs($user)->postJson(
         route('use.proposal.video-intro', [
             'proposal' => $proposal
@@ -79,10 +93,11 @@ test('ProposalVideoIntroUpsert disallowed if temp file does not exist', function
             'uuid' => Str::uuid()
         ]
     );
+    Bus::assertNotDispatched(ConvertVideoForDownloading::class);
 
     $response->assertStatus(422);
     $response->assertJsonValidationErrors(['uuid' => 'Temp file does not exist.']);
-});
+})->only();
 
 test('ProposalVideoIntroUpsert allowed', function () {
     Storage::fake('s3');
@@ -98,6 +113,7 @@ test('ProposalVideoIntroUpsert allowed', function () {
         'organisation' => $user->organisations->first()
     ]);
 
+    Bus::fake();
     $response = actingAs($user)->postJson(
         route('use.proposal.video-intro', [
             'proposal' => $proposal
@@ -107,13 +123,20 @@ test('ProposalVideoIntroUpsert allowed', function () {
         ]
     );
     $response->assertCreated();
-    $response->assertJsonStructure(['data' => ['uuid', 'full_url']]);
+    $response->assertJsonStructure(['data' => ['uuid']]);
+
+    $createdVideo = Video::findByUuid($response->json('data.uuid'));
+
+    assertEquals('proposal', $createdVideo->model_type);
+    assertEquals($proposal->id, $createdVideo->model_id);
+
+    Bus::assertDispatched(ConvertVideoForDownloading::class);
 
     // Assert that the temp file is deleted
-    Storage::disk('s3')->assertMissing("tmp/{$alreadyUploadedUuid}");
+    // Storage::disk('s3')->assertMissing("tmp/{$alreadyUploadedUuid}");
 
     // TODO assert that it was added to the media library/new location
 
     // // Cleanup
     Storage::fake('s3');
-});
+})->only();
