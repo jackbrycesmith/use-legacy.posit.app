2cf17d1459467e2bad6fd0438f2145124b67ba73
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-09-12 00:04:09 +0100

    add frosted glass poster collapsed effect #51

--- resources/js/Components/ProposalIntroVideoRecording.vue
+++ resources/js/Components/ProposalIntroVideoRecording.vue
@@ -16,8 +16,24 @@
         <!-- Example empty icon for new video... -->
         <IconVideoMessage v-if="currentState.matches('collapsed.empty')" class="h-8 w-8 text-red-400" />
 
-        <!-- Example play button for existing video... -->
-        <svg v-if="currentState.matches('collapsed.existing')" class="h-8 w-8 text-red-400" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" role="img"  width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512" style="transform: rotate(360deg);"><path d="M256 48C141.1 48 48 141.1 48 256s93.1 208 208 208 208-93.1 208-208S370.9 48 256 48zm83.8 211.9l-137.2 83c-2.9 1.8-6.7-.4-6.7-3.9V173c0-3.5 3.7-5.7 6.7-3.9l137.2 83c2.9 1.7 2.9 6.1 0 7.8z" fill="currentColor"></path></svg>
+        <template v-if="currentState.matches('collapsed.existing')">
+          <div class="relative w-full h-full rounded-full flex items-center justify-center">
+            <!-- Poster... -->
+            <img
+              :src="proposal.intro_video.poster_url"
+              style="width: 96%; height: 96%;"
+              class="absolute inset-auto rounded-full"/>
+
+            <!-- Frosted glass effect -->
+            <div
+              style="background-color: rgba(255,255,255,0.5); backdrop-filter: blur(2px);"
+              class="absolute inset-auto rounded-full w-full h-full border-4 border-white"/>
+
+            <!-- Example play button for existing video... -->
+            <svg class="h-8 w-8 text-red-400" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" role="img"  width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512" style="transform: rotate(360deg);"><path d="M256 48C141.1 48 48 141.1 48 256s93.1 208 208 208 208-93.1 208-208S370.9 48 256 48zm83.8 211.9l-137.2 83c-2.9 1.8-6.7-.4-6.7-3.9V173c0-3.5 3.7-5.7 6.7-3.9l137.2 83c2.9 1.7 2.9 6.1 0 7.8z" fill="currentColor"></path></svg>
+          </div>
+        </template>
+
 
         <IconHeroiconsSpinner
           v-if="currentState.matches('collapsed.uploading')"
