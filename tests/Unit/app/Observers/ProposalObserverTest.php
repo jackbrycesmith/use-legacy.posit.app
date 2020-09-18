7d1071a79bb03493adaf796cdcdce078257b3d06
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-09-18 13:41:53 +0100

    fix proposal observer test

--- tests/Unit/app/Observers/ProposalObserverTest.php
+++ tests/Unit/app/Observers/ProposalObserverTest.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Models\Organisation;
 use App\Models\Proposal;
+use App\Models\Team;
 
 test('it sets proposal status to draft by default when created', function () {
-    $org = factory(Organisation::class)->create();
-    $proposal = factory(Proposal::class)->create(['organisation_id' => $org->id]);
+    $team = Team::factory()->create();
+    $proposal = Proposal::factory()->create(['team_id' => $team->id]);
 
     assertEquals(Proposal::STATUS_DRAFT, $proposal->status);
     $this->assertDatabaseHas('statuses', [
