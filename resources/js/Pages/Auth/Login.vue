969074e1981238df5ea479443561b7f6345ff2db
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-09-21 14:27:45 +0100

    change case

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-yellow-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full">
+      <div>
+        <img class="mx-auto h-12 w-auto" src="/posit-icon-2-grey-gold.svg" alt="Posit.app logo">
+        <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+          Log in to Posit.app
+        </h2>
+        <p class="mt-2 text-center text-sm leading-5 text-gray-600">
+          Or
+          <inertia-link :href="$route('register')" class="font-medium text-yellow-400 hover:text-yellow-300 focus:outline-none focus:underline transition ease-in-out duration-150">
+            create your free account
+          </inertia-link>
+        </p>
+      </div>
+
+      <NewLoginForm class="mt-8"/>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+import NewLoginForm from '@/Forms/NewLoginForm'
+
+export default {
+  components: { NewLoginForm },
+  metaInfo: () => ({
+    htmlAttrs: {
+      class: ['h-full', 'bg-yellow-50']
+      // TODO this removes existing class attributes on html... so maybe remove from app.blade.php & then follow the rule of allowing to ovveride bodyAttrs for classes?
+    }
+  }),
+}
+</script>
