10ae1f2bf738e24ff0d039510c8f4ff3ccddfc5c
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-09-22 12:56:57 +0100

    missed for previous commit

--- resources/js/Forms/LoginForm.vue
+++ resources/js/Forms/LoginForm.vue
@@ -18,7 +18,7 @@
             aria-label="Email address"
             name="email"
             type="email"
-            :disabled="isSubmitting"
+            :disabled="form.processing"
             autocomplete="username"
             class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
             placeholder="Email address"
@@ -31,7 +31,7 @@
             name="current-password"
             type="password"
             :minlength="minPasswordLength"
-            :disabled="isSubmitting"
+            :disabled="form.processing"
             autocomplete="current-password"
             class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
             placeholder="Password"
@@ -43,10 +43,10 @@
       <div class="mt-6">
         <button
           type="submit"
-          :disabled="isSubmitting"
-          :class="{ 'cursor-not-allowed': isSubmitting }"
+          :disabled="form.processing"
+          :class="{ 'cursor-not-allowed': form.processing }"
           class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-gray-800 bg-yellow-300 hover:bg-yellow-200 focus:outline-none focus:border-yellow-400 focus:shadow-outline-yellow active:bg-yellow-400 transition duration-150 ease-in-out">
-          <span v-if="isSubmitting" class="absolute left-0 inset-y-0 flex items-center pl-3">
+          <span v-if="form.processing" class="absolute left-0 inset-y-0 flex items-center pl-3">
             <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
