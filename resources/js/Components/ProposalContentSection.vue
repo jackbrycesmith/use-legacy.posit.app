8c9a7e35057fbc75c9ea3a5ee26fae4dd0d1a4cc
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-09-23 17:05:59 +0100

    move editor to ProposalContentSection

---
+++ resources/js/Components/ProposalContentSection.vue
@@ -0,0 +1,181 @@
+<template>
+  <editor-content
+    :editor="editor"
+    @handleControlHitUp="handleControlHitUp"
+    @handleControlHitDown="handleControlHitDown"
+    @handleAddBlockAbove="handleAddBlockAbove"
+    @handleAddBlockBelow="handleAddBlockBelow"
+    @handleDeleteBlock="handleDeleteBlock" />
+</template>
+
+<script>
+import { Editor, EditorContent, EditorFloatingMenu } from 'tiptap'
+import {
+  Blockquote,
+  BulletList,
+  CodeBlock,
+  Image,
+  HardBreak,
+  Heading,
+  ListItem,
+  OrderedList,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+  Focus,
+  // HorizontalRule,
+} from 'tiptap-extensions'
+import PositBlockNode from '@/PositEditor/Nodes/PositBlockNode'
+import PositLayoutDocOne from '@/PositEditor/Nodes/PositLayoutDocOne'
+
+export default {
+  components: {
+    EditorContent,
+    EditorFloatingMenu,
+  },
+  data () {
+    return {
+      editor: new Editor({
+        editorProps: {
+          attributes: {
+            class: 'outline-none space-y-2/12'
+          }
+        },
+        extensions: [
+          new PositLayoutDocOne(),
+          new PositBlockNode(),
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new ListItem(),
+          new OrderedList(),
+          // new HorizontalRule(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Image(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Strike(),
+          new Underline(),
+          new History(),
+          new Focus({
+            className: 'has-focus',
+            nested: false,
+          })
+        ],
+        onInit: this.onEditorInit,
+        onTransaction: this.onEditorTransaction,
+        onUpdate: this.onEditorUpdate,
+        onFocus: this.onEditorFocus,
+        onBlur: this.onEditorBlur,
+        onPaste: this.onEditorPaste,
+        onDrop: this.onEditorDrop,
+      })
+    }
+  },
+  methods: {
+    onEditorInit ({ state, view }) {
+      console.log('onEditorInit')
+      console.log(state)
+      console.log(view)
+    },
+    onEditorUpdate ({ state, getHTML, getJSON, transaction }) {
+      console.log('onEditorUpdate')
+      console.log(state, transaction)
+      console.log(getHTML(), getJSON())
+    },
+    onEditorFocus ({ event,  state, view }) {
+      console.log('onEditorFocus')
+      console.log(event, state, view)
+    },
+    onEditorBlur ({ event,  state, view }) {
+      console.log('onEditorBlur')
+      console.log(event, state, view)
+    },
+    onEditorPaste () {
+      console.log(`New content was added from the user's clipboard!`)
+    },
+    onEditorDrop (view, event, slice, moved) {
+      console.log(`onEditorDrop`)
+      console.log(view, event, slice, moved)
+    },
+    onEditorTransaction (params) {
+      console.log(`onEditorTransaction`)
+      console.log(params)
+    },
+    handleControlHitUp ({ node, view, startPos }) {
+      console.log(`handleControlHitUp`, node, view, startPos)
+      const pos = startPos
+      const { nodeBefore } = view.state.doc.resolve(pos)
+
+      if (!nodeBefore) {
+        console.log('no node before...')
+        return
+      }
+
+      // Do prosemirror transaction to move node up!
+      const newPosition = pos - nodeBefore.nodeSize;
+      const from = pos
+      const to = pos + node.nodeSize
+      console.log('newPosition: ', newPosition)
+      console.log('from: ', from)
+      console.log('to: ', to)
+
+      const transactionMoveNodeUp = view.state.tr
+        .replace(from, to)
+        .insert(newPosition, node)
+
+      console.log(transactionMoveNodeUp)
+
+      view.dispatch(transactionMoveNodeUp)
+    },
+    handleControlHitDown ({ node, view, startPos }) {
+      // TODO this whole thing is broken and I dont know why...
+      console.log(`handleControlHitDown`, node, view, startPos)
+      const pos = startPos
+
+      const childAfter = view.state.doc.childAfter(startPos + node.nodeSize)
+      console.log('childAfter: ', childAfter)
+
+      if (childAfter.node == null) {
+        console.log('cannot move down because its the last one/no node after...')
+        return
+      }
+
+      const transactionMoveNodeDown = view.state.tr
+        .replace(pos, pos + node.nodeSize)
+        .insert(pos + childAfter.node.nodeSize, node)
+
+      view.dispatch(transactionMoveNodeDown)
+    },
+    handleAddBlockAbove ({ node, view, startPos }) {
+      console.log('handleAddBlockAbove')
+      const transaction = view.state.tr.insert(startPos, this.editor.schema.node("posit_block", null, [this.editor.schema.node("paragraph")]))
+      view.dispatch(transaction)
+    },
+    handleAddBlockBelow ({ node, view, startPos }) {
+      console.log('handleAddBlockBelow: ', node, view, startPos)
+      const transaction = view.state.tr.insert(startPos + node.nodeSize, this.editor.schema.node("posit_block", null, [this.editor.schema.node("paragraph")]))
+      view.dispatch(transaction)
+    },
+    handleDeleteBlock ({ node, view, startPos }) {
+      console.log('handleDeleteBlock: ', node, view, startPos)
+      const transaction = view.state.tr.replace(startPos, startPos + node.nodeSize)
+      view.dispatch(transaction)
+    },
+  },
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>
--- resources/js/Pages/Use/ProposalView.vue
+++ resources/js/Pages/Use/ProposalView.vue
@@ -3,22 +3,14 @@
     <ProposalOpeningSection
       class="mb-36"
       :proposal.sync="proposal__"
-      @edit-title-done="handleEditTitleDone"
-    />
+      @edit-title-done="handleEditTitleDone"/>
 
-    <editor-content
-      :editor="editor"
-      @handleControlHitUp="handleControlHitUp"
-      @handleControlHitDown="handleControlHitDown"
-      @handleAddBlockAbove="handleAddBlockAbove"
-      @handleAddBlockBelow="handleAddBlockBelow"
-      @handleDeleteBlock="handleDeleteBlock"
-    />
+    <ProposalContentSection
+      ref="content"/>
 
     <ProposalClosingSection
       class="mt-36"
-      :proposal="proposal__"
-    />
+      :proposal="proposal__"/>
 
     <ProposalBackHome
       class="fixed top-5 left-5"/>
@@ -27,11 +19,11 @@
       ref="proposalSlideOver"
       :proposal.sync="proposal__"
       @updatePressed="handleUpdatePressed"/>
+
+    <!-- Modals -->
     <FirstWelcomeModal ref="firstWelcomeModal"/>
     <LoginModal ref="loginModal"/>
-
-    <portal-target name="proposal-view-portal">
-    </portal-target>
+    <portal-target name="proposal-view-portal" />
   </fragment>
 </template>
 
@@ -39,38 +31,23 @@
 import ProposalSlideOver from '@/SlideOvers/ProposalSlideOver'
 import FirstWelcomeModal from '@/Modals/FirstWelcomeModal'
 import ProposalOpeningSection from '@/Components/ProposalOpeningSection'
+import ProposalContentSection from '@/Components/ProposalContentSection'
 import ProposalClosingSection from '@/Components/ProposalClosingSection'
 import LoginModal from '@/Modals/LoginModal'
-import { Editor, EditorContent, EditorFloatingMenu } from 'tiptap'
-import {
-  Blockquote,
-  BulletList,
-  CodeBlock,
-  Image,
-  HardBreak,
-  Heading,
-  ListItem,
-  OrderedList,
-  TodoItem,
-  TodoList,
-  Bold,
-  Code,
-  Italic,
-  Link,
-  Strike,
-  Underline,
-  History,
-  Focus,
-  // HorizontalRule,
-} from 'tiptap-extensions'
-import PositBlockNode from '@/PositEditor/Nodes/PositBlockNode'
-import PositLayoutDocOne from '@/PositEditor/Nodes/PositLayoutDocOne'
 import Proposal from '@/models/Proposal'
 import proposalViewStore from '@/stores/proposalViewStore'
 import ProposalBackHome from '@/Components/ProposalBackHome'
 
 export default {
-  components: { ProposalSlideOver, FirstWelcomeModal, LoginModal, EditorContent, EditorFloatingMenu, ProposalOpeningSection, ProposalClosingSection, ProposalBackHome },
+  components: {
+    ProposalSlideOver,
+    FirstWelcomeModal,
+    LoginModal,
+    ProposalOpeningSection,
+    ProposalContentSection,
+    ProposalClosingSection,
+    ProposalBackHome
+  },
   props: {
     proposal: { type: Object }
   },
@@ -84,47 +61,6 @@ export default {
   data () {
     return {
       proposal__: Proposal.make(),
-      keepInBounds: true,
-      editor: new Editor({
-        editorProps: {
-          attributes: {
-            class: 'outline-none space-y-2/12'
-          }
-        },
-        extensions: [
-          new PositLayoutDocOne(),
-          new PositBlockNode(),
-          new Blockquote(),
-          new BulletList(),
-          new CodeBlock(),
-          new HardBreak(),
-          new Heading({ levels: [1, 2, 3] }),
-          new ListItem(),
-          new OrderedList(),
-          // new HorizontalRule(),
-          new TodoItem(),
-          new TodoList(),
-          new Link(),
-          new Image(),
-          new Bold(),
-          new Code(),
-          new Italic(),
-          new Strike(),
-          new Underline(),
-          new History(),
-          new Focus({
-            className: 'has-focus',
-            nested: false,
-          })
-        ],
-        onInit: this.onEditorInit,
-        onTransaction: this.onEditorTransaction,
-        onUpdate: this.onEditorUpdate,
-        onFocus: this.onEditorFocus,
-        onBlur: this.onEditorBlur,
-        onPaste: this.onEditorPaste,
-        onDrop: this.onEditorDrop,
-      })
     }
   },
   computed: {
@@ -138,146 +74,35 @@ export default {
   mounted () {
     // TODO maybe do component dynamic import loads inside created () or something so we can do stuff when its loaded
     setTimeout(() => {
-      // this.$refs.firstWelcomeModal.show()
       this.$refs.proposalSlideOver.show()
     }, 1200)
-
-    console.log(this.$page.user)
-
-    setTimeout(() => {
-      // this.$refs.firstWelcomeModal.show()
-      // this.$refs.loginModal.show()
-    }, 2000)
-
   },
   watch: {
     proposal: {
       immediate: true,
       handler (value) {
         this.proposal__ = Proposal.make(value)
-        this.editor.setContent(
-          // `
-          //   <div data-posit-type="posit_block">
-          //     Test content 1...
-          //   </div>
 
-          //   <div data-posit-type="posit_block">
-          //     Test content 2...
-          //   </div>
-
-          //   <div data-posit-type="posit_block">
-          //     Test content 3...
-          //   </div>
-          // `
-          this.proposal__.content?.content
-        )
+        this.$nextTick(() => {
+          this.$refs.content.editor.setContent(
+            this.proposal__.content?.content
+          )
+        })
       }
     }
   },
   methods: {
-    onEditorInit ({ state, view }) {
-      console.log('onEditorInit')
-      console.log(state)
-      console.log(view)
-    },
-    onEditorUpdate ({ state, getHTML, getJSON, transaction }) {
-      console.log('onEditorUpdate')
-      console.log(state, transaction)
-      console.log(getHTML(), getJSON())
-    },
-    onEditorFocus ({ event,  state, view }) {
-      console.log('onEditorFocus')
-      console.log(event, state, view)
-    },
-    onEditorBlur ({ event,  state, view }) {
-      console.log('onEditorBlur')
-      console.log(event, state, view)
-    },
-    onEditorPaste () {
-      console.log(`New content was added from the user's clipboard!`)
-    },
-    onEditorDrop (view, event, slice, moved) {
-      console.log(`onEditorDrop`)
-      console.log(view, event, slice, moved)
-    },
-    onEditorTransaction (params) {
-      console.log(`onEditorTransaction`)
-      console.log(params)
-    },
     async handleUpdatePressed () {
       // TODO this is not production ready
       const response = this.$http.put(
         this.$route('use.submit.upsert-proposal-content', { proposal: this.proposalUuid }),
-        this.editor.getJSON()
+        this.$refs.content.editor.getJSON()
       )
       console.log(response)
-    },
-    handleControlHitUp ({ node, view, startPos }) {
-      console.log(`handleControlHitUp`, node, view, startPos)
-      const pos = startPos
-      const { nodeBefore } = view.state.doc.resolve(pos)
-
-      if (!nodeBefore) {
-        console.log('no node before...')
-        return
-      }
-
-      // Do prosemirror transaction to move node up!
-      const newPosition = pos - nodeBefore.nodeSize;
-      const from = pos
-      const to = pos + node.nodeSize
-      console.log('newPosition: ', newPosition)
-      console.log('from: ', from)
-      console.log('to: ', to)
-
-      const transactionMoveNodeUp = view.state.tr
-        .replace(from, to)
-        .insert(newPosition, node)
-
-      console.log(transactionMoveNodeUp)
-
-      view.dispatch(transactionMoveNodeUp)
-    },
-    handleControlHitDown ({ node, view, startPos }) {
-      // TODO this whole thing is broken and I dont know why...
-      console.log(`handleControlHitDown`, node, view, startPos)
-      const pos = startPos
-
-      const childAfter = view.state.doc.childAfter(startPos + node.nodeSize)
-      console.log('childAfter: ', childAfter)
-
-      if (childAfter.node == null) {
-        console.log('cannot move down because its the last one/no node after...')
-        return
-      }
-
-      const transactionMoveNodeDown = view.state.tr
-        .replace(pos, pos + node.nodeSize)
-        .insert(pos + childAfter.node.nodeSize, node)
-
-      view.dispatch(transactionMoveNodeDown)
-    },
-    handleAddBlockAbove ({ node, view, startPos }) {
-      console.log('handleAddBlockAbove')
-      const transaction = view.state.tr.insert(startPos, this.editor.schema.node("posit_block", null, [this.editor.schema.node("paragraph")]))
-      view.dispatch(transaction)
-    },
-    handleAddBlockBelow ({ node, view, startPos }) {
-      console.log('handleAddBlockBelow: ', node, view, startPos)
-      const transaction = view.state.tr.insert(startPos + node.nodeSize, this.editor.schema.node("posit_block", null, [this.editor.schema.node("paragraph")]))
-      view.dispatch(transaction)
-    },
-    handleDeleteBlock ({ node, view, startPos }) {
-      console.log('handleDeleteBlock: ', node, view, startPos)
-      const transaction = view.state.tr.replace(startPos, startPos + node.nodeSize)
-      view.dispatch(transaction)
     },
     handleEditTitleDone (value) {
 
     }
-  },
-  beforeDestroy() {
-    this.editor.destroy()
   },
 }
 </script>
