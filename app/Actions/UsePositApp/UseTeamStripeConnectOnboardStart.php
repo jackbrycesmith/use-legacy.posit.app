19a1d80e5c19cc609b3ca79967211b459b73358d
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-10-02 00:13:23 +0100

    refactor stripe connect onboard start

--- app/Actions/UsePositApp/UseTeamStripeConnectOnboardStart.php
+++ app/Actions/UsePositApp/UseTeamStripeConnectOnboardStart.php
@@ -2,33 +2,41 @@
 
 namespace App\Actions\UsePositApp;
 
-use App\Models\Organisation;
 use App\Models\StripeAccount;
+use App\Models\Team;
 use App\Utils\Constant;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Request;
 use Lorisleiva\Actions\Action;
 
-class UseOrgStripeConnectOnboardStart extends Action
+class UseTeamStripeConnectOnboardStart extends Action
 {
+    /**
+     * Add routes for this action.
+     *
+     * @param \Illuminate\Routing\Router $router The router
+     *
+     * @return void
+     */
     public static function routes(Router $router)
     {
         $router->domain(use_posit_domain())
-            ->middleware(['web', 'auth'])
-            ->get('/org/{org:uuid}/stripe-connect-onboard-start', static::class)
-            ->where('org', Constant::PATTERN_UUID)
-            ->name('use.org.stripe-connect-onboard-start');
+            ->middleware(['web', 'auth:sanctum', 'verified'])
+            ->get('/team/{team:uuid}/stripe-connect-onboard-start', static::class)
+            ->where('team', Constant::PATTERN_UUID)
+            ->name('use.team.stripe-connect-onboard-start');
     }
 
     /**
      * Determine if the user is authorized to make this action.
      *
+     * @param \App\Models\Team $team The team
+     *
      * @return bool
      */
-    public function authorize(Organisation $org)
+    public function authorize(Team $team)
     {
-        // TODO more complex auth
-        return $this->can('update', $org);
+        return $this->can('update', $team);
     }
 
     /**
@@ -46,24 +54,34 @@ class UseOrgStripeConnectOnboardStart extends Action
      *
      * @return mixed
      */
-    public function handle(Organisation $org)
+    public function handle(Team $team)
     {
         // https://stripe.com/docs/connect/connect-onboarding-standard
 
         // 2. Create/Existing Stripe Account
-        $stripeAccount = $org->stripeAccount ?? StripeAccount::createFromStripeApi($org->id);
+        $stripeAccount = $team->stripeAccount ?? StripeAccount::createFromStripeApi($team->id);
 
         // TODO check & stop if already connected?
         // TODO handle disconnected old account/how softdeletes works...
 
         // 3. Call the Account Links API
         $accountLink = $stripeAccount->stripe()->accountLinks()->create(
-            $refreshUrl = route('use.org.stripe-connect-onboard-start', ['org' => $org]),
-            $returnUrl = route('use.org.view', ['org' => $org])
+            $refreshUrl = route('use.team.stripe-connect-onboard-start', ['team' => $team]),
+            $returnUrl = route('use.team.settings', ['team' => $team])
         );
 
-        $redirectUrl = $accountLink->url;
+        return $accountLink->url;
+    }
 
+    /**
+     * The action http response.
+     *
+     * @param string $redirectUrl The redirect url
+     *
+     * @return Illuminate\Http\Response
+     */
+    public function response(string $redirectUrl)
+    {
         if (Request::inertia()) {
             return response('', 409)->header('X-Inertia-Location', $redirectUrl);
         }
