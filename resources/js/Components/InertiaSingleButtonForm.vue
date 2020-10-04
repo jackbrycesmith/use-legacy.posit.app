2b569cc7140db7b0033967136aebf8f019087ce2
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-10-04 15:57:05 +0100

    add optional confirm modal to single button form submit

--- resources/js/Components/InertiaSingleButtonForm.vue
+++ resources/js/Components/InertiaSingleButtonForm.vue
@@ -8,11 +8,33 @@
         <slot name="button-inner" v-bind="{ form }" />
       </button>
     </slot>
+
+    <ConfirmModal
+      ref="confirmModal"
+      v-if="shouldConfirm && hasJustRequestedConfirm"
+      :titleText="confirmModalTitle"
+      :description-text="confirmModalDescription"
+      :confirm-button-text="confirmModalButton"
+      :confirm-button-colors="confirmModalButtonColors"
+      :hint-icon-bg-color="confirmModalHintIconBgColor"
+      @confirmed="submitForm"
+      @cancelled="handleConfirmCancelled">
+
+      <template #confirm-hint-icon>
+        <slot name="confirm-modal-icon" />
+      </template>
+
+    </ConfirmModal>
   </form>
 </template>
 
 <script>
+import ConfirmModal from '@/Modals/ConfirmModal'
+
 export default {
+  components: {
+    ConfirmModal
+  },
   props: {
     route: { type: String, required: true },
     buttonClass: { type: String },
@@ -28,16 +50,55 @@ export default {
     errorBag: {
       type: String,
       default: 'default'
+    },
+    shouldConfirm: {
+      type: Boolean,
+      default: false
+    },
+    confirmModalTitle: {
+      type: String,
+      default: 'Confirm'
+    },
+    confirmModalDescription: {
+      type: String,
+      default: 'Are you sure you want to do this action?'
+    },
+    confirmModalButton: {
+      type: String,
+      default: 'Okay'
+    },
+    confirmModalButtonColors: {
+      type: String,
+      default: 'border border-transparent bg-red-400 text-white hover:bg-red-300 focus:outline-none focus:border-red-500 focus:shadow-outline-red'
+    },
+    confirmModalHintIconBgColor: {
+      type: String,
+      default: 'bg-red-100'
     }
   },
   data () {
     return {
-      form: this.$inertia.form({}, { bag: this.errorBag })
+      form: this.$inertia.form({}, { bag: this.errorBag }),
+      hasJustRequestedConfirm: false
     }
   },
   methods: {
-    handleFormSubmit () {
+    submitForm () {
       this.form.submit(this.requestMethod, this.route, this.requestOptions)
+    },
+    handleFormSubmit () {
+      if (this.shouldConfirm) {
+        this.hasJustRequestedConfirm = true
+        this.$nextTick(() => {
+          this.$refs.confirmModal.show()
+        })
+        return
+      }
+
+      this.submitForm()
+    },
+    handleConfirmCancelled () {
+      //
     }
   }
 }
