3750a9b298a6e19b9207c4c432adab426b5ef61e
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-10-13 15:51:20 +0100

    add InputWithCurrency reusable tailwindui #111

---
+++ resources/js/Components/InputCleave.vue
@@ -0,0 +1,36 @@
+<template>
+  <input type="text" @keyup="updateValue">
+</template>
+
+<script>
+import Cleave from 'cleave.js'
+
+export default {
+  props: {
+    value: {},
+    options: {},
+  },
+  mounted() {
+   this.cleave = new Cleave(this.$el, this.options)
+   this.cleave.setRawValue(this.value)
+  },
+  destroyed() {
+    this.cleave.destroy()
+  },
+  watch: {
+    value: 'updateInput'
+  },
+  methods: {
+    updateValue() {
+      var val = this.cleave.getRawValue()
+
+      if (val !== this.value) {
+        this.$emit('input', val)
+      }
+    },
+    updateInput() {
+      this.cleave.setRawValue(this.value)
+    }
+  }
+}
+</script>
---
+++ resources/js/Components/TailwindUI/InputWithCurrency.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <label :for="inputLabelId" class="block text-sm leading-5 font-medium text-gray-600">
+      {{ label }}
+    </label>
+    <div class="mt-1 relative rounded-md shadow-sm">
+      <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <span class="text-gray-500 sm:text-sm sm:leading-5">
+          {{ selectedCurrency.symbol_native }}
+        </span>
+      </div>
+
+      <InputCleave
+        :id="inputLabelId"
+        v-model="amount"
+        :options="{
+          numeral: true,
+          numeralPositiveOnly: true,
+          numeralThousandsGroupStyle: 'thousand'
+        }"
+        class="form-input block w-full pl-7 pr-12 sm:text-sm sm:leading-5"
+        placeholder="0.00"
+        />
+
+      <div class="absolute inset-y-0 right-0 flex items-center">
+        <select
+          v-model="selectedCurrencyCode"
+          aria-label="Currency"
+          class="form-select h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm sm:leading-5 cursor-pointer">
+
+          <option
+            v-for="currency in currencies"
+            :key="currency.code"
+            :value="currency.code">
+            {{ currency.code }} {{ currency.emoji }} &nbsp;
+          </option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputCleave from '@/Components/InputCleave'
+import { isNil, find, get, debounce, snakeCase } from 'lodash-es'
+
+export default {
+  components: {
+    InputCleave,
+  },
+  props: {
+    label: {
+      type: String,
+      default: 'Price'
+    },
+    defaultCurrencyCode: {
+      type: String,
+      default: 'GBP',
+    },
+    currencies: {
+      type: Array,
+      default: () => [
+        {
+          code: 'GBP',
+          symbol_native: '£',
+          emoji: '🇬🇧'
+        },
+        {
+          code: 'USD',
+          symbol_native: '$',
+          emoji: '🇺🇸'
+        },
+        {
+          code: 'EUR',
+          symbol_native: '€',
+          emoji: '🇪🇺'
+        },
+        {
+          code: 'AUD',
+          symbol_native: '$',
+          emoji: '🇦🇺'
+        },
+        {
+          code: 'CAD',
+          symbol_native: '$',
+          emoji: '🇨🇦'
+        },
+        {
+          code: 'NZD',
+          symbol_native: '$',
+          emoji: '🇳🇿'
+        },
+      ]
+    },
+    currencyModel: {
+      type: String
+    },
+    amountModel: {
+      type: [ String, Number ]
+    },
+    notifyChangeDebounceMs: {
+      type: Number,
+      default: 1000
+    },
+    min: Number,
+    max: Number
+  },
+  created () {
+    this.notifyChange = debounce((vm) => {
+      vm.$emit('changed')
+    }, this.notifyChangeDebounceMs)
+  },
+  computed: {
+    inputLabelId () {
+      return snakeCase(this.label)
+    },
+    selectedCurrency () {
+      return find(this.currencies, { code: this.selectedCurrencyCode })
+    },
+    amount: {
+      get () {
+        return this.amountModel
+      },
+      set (value) {
+        this.$emit('update:amountModel', value)
+        this.notifyChange(this)
+      }
+    },
+    selectedCurrencyCode: {
+      get () {
+        if (isNil(this.currencyModel)) {
+          return this.defaultCurrencyCode
+        }
+
+        return this.currencyModel
+      },
+      set (value) {
+        this.$emit('update:currencyModel', value)
+        this.notifyChange(this)
+      }
+    }
+  },
+}
+</script>
