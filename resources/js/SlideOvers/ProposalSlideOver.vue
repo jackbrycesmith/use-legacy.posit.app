43e745a1caa37a889a7fc775112a568438df6cfa
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-10-15 23:50:08 +0100

    add tabs start to proposalslideover #114

--- resources/js/SlideOvers/ProposalSlideOver.vue
+++ resources/js/SlideOvers/ProposalSlideOver.vue
@@ -103,43 +103,55 @@
       <template #body>
         <div class="flex-1 flex flex-col justify-between">
           <div class="px-4 sm:px-6">
-            <!-- Section -->
-            <div class="space-y-6 pt-6 pb-5 mt-5">
+            <Tabs
+              :show-tab-text-when-inactive="false"
+              active-tab-class="border-b-2 border-primary-yellow-500 text-primary-yellow-600 focus:outline-none focus:text-primary-yellow-800 focus:border-primary-yellow-700"
+              active-tab-icon-class="text-primary-yellow-500 group-focus:text-primary-yellow-600"
+              tabs-class="mt-8">
 
-              <!-- Project Value -->
-              <InputWithCurrency
-                :currency-model.sync="proposal.value_currency_code"
-                :amount-model.sync="proposal.value_amount"
-                :max="999999999"
-                label="Project Value"
-                @changed="handleUpdateProjectValue"
-                class="space-y-1" />
+              <TabPane title="Design" :icon="designTabIcon">
+                <div class="space-y-6 pt-6 pb-5">
 
-              <div class="space-y-1">
-                <label for="proposal_theme" class="block text-sm font-medium leading-5 text-gray-600">
-                  Theme
-                </label>
+                  <!-- Proposal Theme -->
+                  <div class="space-y-1">
+                    <label for="proposal_theme" class="block text-sm font-medium leading-5 text-gray-600">
+                      Theme
+                    </label>
 
-                <!-- Theme list options -->
-                <div class="flex gap-2 items-center">
-                  <ProposalThemeBlock name="Cool Grey" class="flex-shrink-0" />
-                  <p class="flex-1 text-center text-gray-400 text-xs">
-                    <IconHeroiconsMediumInformationCircle class="inline w-5 h-5 align-bottom" />
-                    More theme choices coming soon!
-                  </p>
+                    <!-- Theme list options -->
+                    <div class="flex gap-2 items-center">
+                      <ProposalThemeBlock name="Cool Grey" class="flex-shrink-0" />
+                      <p class="flex-1 text-center text-gray-400 text-xs">
+                        <IconHeroiconsMediumInformationCircle class="inline w-5 h-5 align-bottom" />
+                        More theme choices coming soon!
+                      </p>
+                    </div>
+                  </div>
+
                 </div>
-              </div>
+              </TabPane>
 
-            </div>
+              <TabPane title="Details" :icon="detailsTabIcon">
 
-            <!-- Another section -->
-            <div class="space-y-4 pt-4 pb-6">
-              <!-- Next Actions -->
-              <div class="space-y-2">
+                  <div class="space-y-6 pt-6 pb-5">
 
+                    <!-- Project Value -->
+                    <InputWithCurrency
+                      :currency-model.sync="proposal.value_currency_code"
+                      :amount-model.sync="proposal.value_amount"
+                      :max="999999999"
+                      label="Project Value"
+                      @changed="handleUpdateProjectValue"
+                      class="space-y-1" />
 
-              </div>
-            </div>
+                    <!-- Deposit -->
+                    <ProposalDepositConfigure
+                      class="space-y-1"
+                    />
+
+                  </div>
+              </TabPane>
+            </Tabs>
 
           </div>
         </div>
@@ -227,9 +239,14 @@ import ProposalThemeBlock from '@/Components/ProposalThemeBlock'
 import IconHeroiconsMediumInformationCircle from '@/Icons/IconHeroiconsMediumInformationCircle'
 import IconHeroiconsSmallExternalLink from '@/Icons/IconHeroiconsSmallExternalLink'
 import IconHeroiconsSmallCheck from '@/Icons/IconHeroiconsSmallCheck'
+import IconHeroiconsSmallBriefcase from '@/Icons/IconHeroiconsSmallBriefcase'
+import IconHeroiconsSmallAdjustments from '@/Icons/IconHeroiconsSmallAdjustments'
 import SuccessFlashSwitcher from '@/Components/SuccessFlashSwitcher'
 import BadgeWithDotSmall from '@/Components/TailwindUI/BadgeWithDotSmall'
 import InputWithCurrency from '@/Components/TailwindUI/InputWithCurrency'
+import ProposalDepositConfigure from '@/Components/ProposalDepositConfigure'
+import Tabs from '@/Components/Tabs'
+import TabPane from '@/Components/TabPane'
 import copy from 'clipboard-copy'
 
 export default {
@@ -243,8 +260,13 @@ export default {
     IconHeroiconsMediumInformationCircle,
     IconHeroiconsSmallExternalLink,
     IconHeroiconsSmallCheck,
+    IconHeroiconsSmallBriefcase,
+    IconHeroiconsSmallAdjustments,
     InputWithCurrency,
-    SuccessFlashSwitcher
+    SuccessFlashSwitcher,
+    ProposalDepositConfigure,
+    Tabs,
+    TabPane
   },
   props: {
     proposal: { type: Object }
@@ -252,6 +274,14 @@ export default {
   data: () => ({
     isVisible: false
   }),
+  computed: {
+    detailsTabIcon () {
+      return IconHeroiconsSmallBriefcase
+    },
+    designTabIcon () {
+      return IconHeroiconsSmallAdjustments
+    }
+  },
   methods: {
     handleExtraCloseButtonHit () {
       this.isVisible ? this.hide() : this.show()
