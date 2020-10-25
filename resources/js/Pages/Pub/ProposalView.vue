2f718c6e2f8c1385c80c6355fc9288e8a8fcc6fe
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-10-25 21:42:55 +0000

    quick fix to include UseProposalView -> pub #119

--- resources/js/Pages/Pub/ProposalView.vue
+++ resources/js/Pages/Pub/ProposalView.vue
@@ -1,69 +1,35 @@
 <template>
   <fragment>
-    <div v-if="!is_draft" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
-      <div class="max-w-3xl mx-auto">
-        <!-- Content goes here -->
+    <ProposalOpeningSection
+      class="mb-36"
+      :proposal="proposal__" />
 
-        <div class="bg-white overflow-hidden shadow rounded-lg mt-5">
-          <div class="px-4 py-5 sm:p-6">
-            <!-- Content goes here -->
-            <editor-content :editor="editor"/>
-          </div>
-        </div>
+    <ProposalContentSection ref="content" />
 
-        <div class="text-center mt-5">
-          <span class="inline-flex rounded-md shadow-sm">
-            <button @click="handlePaymentClick" :disabled="stripe === null" type="button" class="inline-flex items-center px-6 py-3 border border-gray-300 text-base leading-6 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
-              Payment
-            </button>
-          </span>
-        </div>
+    <ProposalClosingSection
+      class="mt-36"
+      :proposal="proposal__"/>
 
-      </div>
-    </div>
-
-    <!-- TODO this is naive, should really put in own component -->
-    <div v-else class="h-screen flex justify-center items-center">
-      <div>
-        <img class="h-24 m-auto mb-3" src="/posit-icon.png"/>
-        <h3 class="text-2xl leading-6 font-medium text-gray-900 text-center">
-          Proposal in draft...
-        </h3>
-        <p class="mt-3 text-sm leading-5 text-gray-500 text-center">
-          Please ask the creator to publish.
-        </p>
-      </div>
-    </div>
+    <!-- Modals -->
+    <portal-target name="proposal-view-portal" />
   </fragment>
 </template>
 
 <script>
 import { loadStripe } from '@stripe/stripe-js'
-import { Editor, EditorContent } from 'tiptap'
-
-import {
-  Blockquote,
-  BulletList,
-  CodeBlock,
-  HardBreak,
-  Heading,
-  ListItem,
-  OrderedList,
-  TodoItem,
-  TodoList,
-  Bold,
-  Code,
-  Italic,
-  Link,
-  Strike,
-  Underline,
-  History,
-} from 'tiptap-extensions'
+import Proposal from '@/models/Proposal'
+import ProposalOpeningSection from '@/Components/ProposalOpeningSection'
+import ProposalContentSection from '@/Components/ProposalContentSection'
+import ProposalClosingSection from '@/Components/ProposalClosingSection'
 
 export default {
-  components: { EditorContent },
+  components: {
+    ProposalOpeningSection,
+    ProposalContentSection,
+    ProposalClosingSection
+  },
   props: {
+    status: { type: String },
     proposal: { type: Object },
     is_draft: { type: Boolean, default: false },
     stripe_pub_key: {}
@@ -71,53 +37,45 @@ export default {
   data () {
     return {
       stripe: null,
-      keepInBounds: true,
-      editor: new Editor({
-        editorProps: {
-          attributes: {
-            class: 'prose'
-          }
-        },
-        editable: false,
-        extensions: [
-          new Blockquote(),
-          new BulletList(),
-          new CodeBlock(),
-          new HardBreak(),
-          new Heading({ levels: [1, 2, 3] }),
-          new ListItem(),
-          new OrderedList(),
-          new TodoItem(),
-          new TodoList(),
-          new Link(),
-          new Bold(),
-          new Code(),
-          new Italic(),
-          new Strike(),
-          new Underline(),
-          new History(),
-        ],
-      })
+      proposal__: Proposal.make()
+    }
+  },
+  metaInfo () {
+    return {
+      htmlAttrs: {
+        class: ['h-full', this.htmlBgColorClass]
+      }
+    }
+  },
+  computed: {
+    htmlBgColorClass () {
+      return 'bg-gray-50'
     }
   },
   watch: {
     proposal: {
       immediate: true,
       handler (value) {
-        this.editor.setContent(value.data?.content?.content)
+        this.proposal__ = Proposal.make(value)
+
+        this.$nextTick(() => {
+          this.$refs.content.editor.setContent(
+            this.proposal__.content?.content
+          )
+        })
       }
     }
   },
   mounted () {
-    setTimeout(async () => {
-      let stripe = await loadStripe(this.stripe_pub_key, {
-        stripeAccount: this.proposal.data.stripe_account_id
-      })
-      this.stripe = stripe
-      // (async function(thing =) {
-      // })()
-      console.log(this.proposal, this.stripe_pub_key)
-    }, 1000)
+    // setTimeout(async () => {
+    //   let stripe = await loadStripe(this.stripe_pub_key, {
+    //     stripeAccount: this.proposal.data.stripe_account_id
+    //   })
+    //   this.stripe = stripe
+    //   // (async function(thing =) {
+    //   // })()
+    //   console.log(this.proposal, this.stripe_pub_key)
+    // }, 1000)
   },
   methods: {
     handlePaymentClick () {
