08d8ea703182879bff5a176091996cca9f56b221
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-10-27 20:11:12 +0000

    highlight team navigation current page closes #124

--- resources/js/Components/DashboardTeamNavigation.vue
+++ resources/js/Components/DashboardTeamNavigation.vue
@@ -1,52 +1,74 @@
 <template>
-  <div class="flex flex-col space-y-6 sm:flex-row sm:space-y-0 sm:space-x-8 xl:flex-col xl:space-x-0 xl:space-y-6">
+  <nav class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-8 xl:flex-col xl:space-x-0 xl:space-y-2">
 
-    <div class="flex items-center space-x-2">
+    <!-- Credits -->
+    <div class="group flex items-center rounded-md px-2 py-2 space-x-2">
       <IconCredits class="h-5 w-5 text-gray-400" />
       <span class="text-sm text-gray-500 leading-5 font-medium">&infin; Credits</span>
     </div>
 
-    <!--  -->
-<!--     <div class="flex items-center space-x-2">
-      <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-        <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
-      </svg>
-      <span class="text-sm text-gray-500 leading-5 font-medium">8 Projects</span>
-    </div> -->
-
     <!-- Proposals -->
-    <inertia-link :href="$route('use.index')" class="flex items-center space-x-2">
-      <svg viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5 text-gray-400"><path d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z"></path></svg>
+    <inertia-link
+      :href="$route('use.index')"
+      :class="routeIs('use.index') ? activeNavItemClass : inactiveNavItemClass"
+      class="group flex items-center rounded-md px-2 py-2 space-x-2 focus:outline-none focus:bg-primary-yellow-100 transition ease-in-out duration-150">
+      <IconHeroiconsSmallLightBulb class="h-5 w-5 text-gray-400" />
       <span class="text-sm text-gray-500 leading-5 font-medium">Proposals</span>
     </inertia-link>
 
     <!-- Contacts/Recipients -->
-    <inertia-link :href="org.route_contacts" class="flex items-center space-x-2">
-      <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="vertical-align: -0.125em;-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M19 0H5c-.55 0-1 .45-1 1s.45 1 1 1h14c.55 0 1-.45 1-1s-.45-1-1-1zM5 24h14c.55 0 1-.45 1-1s-.45-1-1-1H5c-.55 0-1 .45-1 1s.45 1 1 1zM20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-8 2.75c1.24 0 2.25 1.01 2.25 2.25s-1.01 2.25-2.25 2.25S9.75 10.24 9.75 9S10.76 6.75 12 6.75zM17 17H7v-1.5c0-1.67 3.33-2.5 5-2.5s5 .83 5 2.5V17z" fill="currentColor"/></svg>
+    <inertia-link
+      :href="org.route_contacts"
+      :class="routeIs('use.team.contacts') ? activeNavItemClass : inactiveNavItemClass"
+      class="group flex items-center rounded-md px-2 py-2 space-x-2 focus:outline-none focus:bg-primary-yellow-100 transition ease-in-out duration-150">
+      <IconContact class="h-5 w-5 text-gray-400" />
       <span class="text-sm text-gray-500 leading-5 font-medium">Contacts</span>
     </inertia-link>
 
     <!-- Settings -->
-    <inertia-link :href="org.route_settings" class="flex items-center space-x-2">
-      <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"></path></svg>
+    <inertia-link
+      :href="org.route_settings"
+      :class="routeIs('use.team.settings') ? activeNavItemClass : inactiveNavItemClass"
+      class="group flex items-center rounded-md px-2 py-2 space-x-2 focus:outline-none focus:bg-primary-yellow-100 transition ease-in-out duration-150">
+      <IconHeroiconsSmallCog class="h-5 w-5 text-gray-400" />
       <span class="text-sm text-gray-500 leading-5 font-medium">Settings</span>
     </inertia-link>
-  </div>
+  </nav>
 </template>
 
 <script>
 import IconCredits from '@/Icons/IconCredits'
+import IconContact from '@/Icons/IconContact'
+import IconHeroiconsSmallCog from '@/Icons/IconHeroiconsSmallCog'
+import IconHeroiconsSmallLightBulb from '@/Icons/IconHeroiconsSmallLightBulb'
 
 export default {
   components: {
-    IconCredits
+    IconCredits,
+    IconContact,
+    IconHeroiconsSmallCog,
+    IconHeroiconsSmallLightBulb
   },
   props: {
-    org: { type: Object }
+    org: { type: Object },
+    url: { type: String }
   },
   data () {
     return {
 
+    }
+  },
+  computed: {
+    activeNavItemClass () {
+      return 'bg-primary-yellow-200 focus:bg-primary-yellow-100'
+    },
+    inactiveNavItemClass () {
+      return 'hover:bg-primary-yellow-100'
+    },
+  },
+  methods: {
+    routeIs (key) {
+      return route().current(key)
     }
   }
 }
