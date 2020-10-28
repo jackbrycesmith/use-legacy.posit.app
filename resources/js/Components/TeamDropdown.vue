74cecfa2cc1551fe977c8200debd0f816f02b33f
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-10-28 13:09:51 +0000

    fix issue with team switcher overflow #125

--- resources/js/Components/TeamDropdown.vue
+++ resources/js/Components/TeamDropdown.vue
@@ -1,23 +1,23 @@
 <template>
   <!-- <div class="flex-1 flex flex-col overflow-y-auto"> -->
     <!-- User account dropdown -->
-    <div class="sm:mt-6 relative text-left">
+    <div class="sm:max-w-sm xl:mt-6 relative text-left">
       <!-- Dropdown menu toggle, controlling the show/hide state of dropdown menu. -->
       <div>
-        <button ref="menuTriggerButton" @click="handleDropdownButtonClick" type="button" class="group w-full rounded-md px-3.5 py-2 text-sm leading-5 font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-500 focus:outline-none focus:bg-gray-200 focus:border-blue-300 active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150" id="options-menu" aria-haspopup="true" :aria-expanded="isOpen">
+        <button ref="menuTriggerButton" @click="handleDropdownButtonClick" type="button" class="group w-full rounded-md px-3.5 py-2 text-sm leading-5 font-medium text-gray-700 hover:bg-cool-gray-50 hover:text-gray-500 focus:outline-none focus:bg-cool-gray-100 focus:border-blue-300 active:bg-cool-gray-50 active:text-gray-800 transition ease-in-out duration-150" id="options-menu" aria-haspopup="true" :aria-expanded="isOpen">
           <div class="flex w-full justify-between items-center">
-            <div class="flex items-center justify-between space-x-3">
-              <div class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0 flex items-center justify-center">
+            <div class="flex min-w-0 items-center justify-between space-x-3">
+              <div class="w-10 h-10 bg-cool-gray-200 rounded-full flex-shrink-0 flex items-center justify-center">
                 {{ org__.avatar_letter_initial }}
               </div>
-              <div class="flex-1">
-                <h2 class="text-gray-900 text-sm leading-5 font-medium text-left">
+              <div class="flex-1 min-w-0">
+                <h2 class="text-gray-900 text-sm leading-5 font-medium text-left truncate">
                   {{ org__.name }}
                 </h2>
-                <p class="text-gray-500 text-sm leading-5 truncate uppercase text-left">personal</p>
+                <p class="text-gray-500 text-sm leading-5 truncate uppercase text-left truncate">personal</p>
               </div>
             </div>
-            <svg class="h-5 w-5 text-gray-400 group-hover:text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+            <svg class="flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500" viewBox="0 0 20 20" fill="currentColor">
               <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
             </svg>
           </div>
