b37be40dcf73792eca491bd6843b896d6a2bc395
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-10-29 00:04:03 +0000

    hide activity & add getting started card wip #126

---
+++ resources/js/Components/GettingStartedWelcome.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="bg-white overflow-hidden shadow rounded-lg">
+    <div class="bg-primary-yellow-200 rounded-t-lg pt-5 pb-2 px-4 sm:px-6 lg:px-8">
+      <h3 class="text-lg leading-6 font-medium text-gray-900">
+        <ApplicationMark class="inline-block h-7 w-7 -ml-1 mr-0.5" /> Getting Started
+      </h3>
+
+      <div class="mt-2 max-w-xl text-sm leading-5 text-gray-500 mb-4">
+        <p>
+          Any issues? » <a class="underline text-black" href="mailto:help@posit.app">help@posit.app</a>
+        </p>
+      </div>
+    </div>
+
+    <div class="py-5 px-4 sm:px-6 lg:px-8">
+      <nav class="flex">
+        <ul class="space-y-6">
+          <li>
+            <!-- Complete Step -->
+            <!-- <a href="#" class="group"> -->
+              <div class="flex items-start space-x-3">
+                <div class="flex-shrink-0 relative h-5 w-5 flex items-center justify-center">
+                  <IconHeroiconsSmallCheckCircle
+                    class="h-full w-full text-primary-yellow-600 group-hover:text-primary-yellow-800 group-focus:text-primary-yellow-800 transition ease-in-out duration-150"
+                  />
+
+                </div>
+                <p class="text-sm leading-5 font-medium text-gray-500 group-hover:text-gray-900 group-focus:text-gray-900 transition ease-in-out duration-150">Create account</p>
+              </div>
+            <!-- </a> -->
+          </li>
+
+          <li>
+            <!-- Current Step -->
+            <component :is="paymentProviderSetupComponent" v-bind="paymentProviderSetupAttrs">
+              <div class="flex items-start space-x-3">
+                <div class="flex-shrink-0 h-5 w-5 relative flex items-center justify-center">
+                  <IconHeroiconsSmallCheckCircle
+                    v-if="hasSetupPaymentProvider"
+                    class="h-full w-full text-primary-yellow-600 group-hover:text-primary-yellow-800 group-focus:text-primary-yellow-800 transition ease-in-out duration-150"
+                  />
+
+                  <template v-else>
+                    <!-- Current one... -->
+                    <span class="absolute h-4 w-4 rounded-full bg-primary-yellow-200"></span>
+                    <span class="relative block w-2 h-2 bg-primary-yellow-600 rounded-full"></span>
+                  </template>
+                </div>
+                <p
+                  :class="hasSetupPaymentProvider ? 'text-gray-500' : 'text-primary-yellow-600'"
+                  class="text-sm leading-5 font-medium">
+                  Setup payments provider
+                </p>
+              </div>
+            </component>
+          </li>
+
+          <li>
+            <!-- Upcoming Step -->
+            <!-- <a href="#" class="group"> -->
+              <div class="flex items-start space-x-3">
+                <div class="flex-shrink-0 h-5 w-5 relative flex items-center justify-center">
+
+                  <template v-if="!hasSetupPaymentProvider && !hasSentFirstProposal">
+                    <span class="block h-2 w-2 bg-gray-300 rounded-full group-hover:bg-gray-400 group-focus:bg-gray-400 transition ease-in-out duration-150"></span>
+                  </template>
+
+                  <template v-if="hasSetupPaymentProvider && !hasSentFirstProposal">
+                    <!-- Current one... -->
+                    <span class="absolute h-4 w-4 rounded-full bg-primary-yellow-200"></span>
+                    <span class="relative block w-2 h-2 bg-primary-yellow-600 rounded-full"></span>
+                  </template>
+
+                  <template v-if="hasSentFirstProposal">
+                    <IconHeroiconsSmallCheckCircle
+                      class="h-full w-full text-primary-yellow-600 group-hover:text-primary-yellow-800 group-focus:text-primary-yellow-800 transition ease-in-out duration-150"
+                    />
+                  </template>
+
+                </div>
+                <p
+                  :class="(!hasSetupPaymentProvider || hasSentFirstProposal ) ? 'text-gray-500' : 'text-primary-yellow-600'"
+                  class="text-sm leading-5 font-medium text-gray-500 group-hover:text-gray-900 group-focus:text-gray-900 transition ease-in-out duration-150">
+                  Send your 1<sup>st</sup> proposal
+                </p>
+              </div>
+            <!-- </a> -->
+          </li>
+
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+import ApplicationMark from '@/Jetstream/ApplicationMark'
+import IconHeroiconsSmallCheckCircle from '@/Icons/IconHeroiconsSmallCheckCircle'
+
+export default {
+  name: 'GettingStartedWelcome',
+  components: {
+    ApplicationMark,
+    IconHeroiconsSmallCheckCircle
+  },
+  props: {
+    team: {
+      type: Object
+    }
+  },
+  computed: {
+    paymentProviderSetupComponent () {
+      return 'inertia-link'
+    },
+    paymentProviderSetupAttrs () {
+      return {
+        href: this.team.route_settings
+      }
+    },
+    hasSetupPaymentProvider () {
+      return this.team?.has_setup_payment_provider ?? false
+    },
+    hasSentFirstProposal () {
+      // TODO check if a proposal has been published under the team
+      return false
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
--- resources/js/Pages/Use/Index.vue
+++ resources/js/Pages/Use/Index.vue
@@ -25,30 +25,11 @@
     <!-- Activity feed -->
     <div class="pr-4 sm:pr-6 lg:pr-8 lg:flex-shrink-0 lg:border-l lg:border-gray-200 xl:pr-0">
       <div class="pl-6 lg:w-80">
-        <div class="pt-6 pb-2">
-          <h2 class="text-sm leading-5 font-semibold">Activity</h2>
-        </div>
-        <div>
-          <ul class="divide-y divide-gray-200">
-<!--             <li class="py-4">
-              <div class="flex space-x-3">
-                <img class="h-6 w-6 rounded-full" src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&h=256&q=80" alt="">
-                <div class="flex-1 space-y-1">
-                  <div class="flex items-center justify-between">
-                    <h3 class="text-sm font-medium leading-5">You</h3>
-                    <p class="text-sm leading-5 text-gray-500">1h</p>
-                  </div>
-                  <p class="text-sm leading-5 text-gray-500">Deployed Workcation (2d89f0c8 in master) to production</p>
-                </div>
-              </div>
-            </li> -->
 
-            <!-- More items... -->
-          </ul>
-          <div class="py-4 text-sm leading-5 border-t border-gray-200">
-            <inertia-link :href="org__.route_activity_logs" class="text-yellow-400 font-semibold hover:text-yellow-300 cursor-pointer">View all activity &rarr;</inertia-link>
-          </div>
-        </div>
+        <GettingStartedWelcome
+          :team="org__"
+          class="mt-6" />
+
       </div>
     </div>
   </div>
@@ -61,13 +42,15 @@ import OrganisationList from '@/Lists/OrganisationList'
 import Dashboard from '@/Layouts/Dashboard'
 import Organisation from '@/models/Organisation'
 import ProposalDashboardListItem from '@/Lists/ProposalDashboardListItem'
+import GettingStartedWelcome from '@/Components/GettingStartedWelcome'
 
 export default {
   components: {
     OrganisationList,
     ProposalList,
     ProposalDashboardListItem,
-    TeamDashboardSidebar
+    TeamDashboardSidebar,
+    GettingStartedWelcome
   },
   props: {
     org: { type: Object, default: () => {} }
