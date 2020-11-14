58e589ba9f1b06d539e24b96a37cc40659c3ea09
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-11-14 00:13:07 +0000

    commonnn

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Monicahq\Cloudflare\Http\Middleware\TrustProxies as Middleware;
+use Fideloper\Proxy\TrustProxies as Middleware;
+// use Monicahq\Cloudflare\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
@@ -12,7 +13,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = ['*'];
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
