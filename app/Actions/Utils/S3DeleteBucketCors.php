00a9e4c76c382906a18b956962bfe5aa465787db
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-11-16 18:16:37 +0000

    add extra s3 cors utils commands

---
+++ app/Actions/Utils/S3DeleteBucketCors.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Actions\Utils;
+
+use Aws\Exception\AwsException;
+use Aws\S3\S3Client;
+use Illuminate\Console\Command;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Arr;
+use Lorisleiva\Actions\Action;
+
+class S3DeleteBucketCors extends Action
+{
+    protected static $commandSignature = 'utils:s3-delete-bucket-cors
+                                          {disk : The name of the s3 disk in config/filesystems.php}';
+
+    public function getAttributesFromCommand(Command $command): array
+    {
+        return [
+            'disk' => $command->argument('disk'),
+        ];
+    }
+
+    public function asCommand(Command $command)
+    {
+        $this->s3DiskConfig = config("filesystems.disks.{$this->disk}");
+
+        if (Arr::get($this->s3DiskConfig, 'driver') !== 's3') {
+            throw new \Exception("Provided disk [{$this->disk}] is not using the s3 driver.");
+            return;
+        }
+    }
+
+    /**
+     * Execute the action and return a result.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $client = $this->s3Client();
+        $bucketName = Arr::get($this->s3DiskConfig, 'bucket');
+
+        try {
+            $result = $client->deleteBucketCors([
+                'Bucket' => $bucketName, // REQUIRED
+            ]);
+            var_dump($result);
+        } catch (AwsException $e) {
+            // output error message if fails
+            error_log($e->getMessage());
+        }
+    }
+
+    public function consoleOutput($result, Command $command)
+    {
+        // $command->info("Fixed postgres table sequence!");
+    }
+
+    /**
+     * Get the s3 client.
+     *
+     * @return S3Client
+     */
+    protected function s3Client()
+    {
+        return new S3Client([
+            'url' => Arr::get($this->s3DiskConfig, 'endpoint'),
+            'endpoint' => Arr::get($this->s3DiskConfig, 'endpoint'),
+            'region' => Arr::get($this->s3DiskConfig, 'region'),
+            'version' => 'latest',
+            'signature_version' => 'v4',
+            'use_path_style_endpoint' => Arr::get($this->s3DiskConfig, 'use_path_style_endpoint'),
+            'credentials' => [
+                'key' => Arr::get($this->s3DiskConfig, 'key'),
+                'secret' => Arr::get($this->s3DiskConfig, 'secret'),
+            ]
+        ]);
+    }
+}
