82541c11f5a8d7f6f5be18a0ebb43d69e1b43b2f
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-11-24 18:56:59 +0000

    add formatCurrency js util

--- resources/js/utils/strings.js
+++ resources/js/utils/strings.js
@@ -1,4 +1,4 @@
-import { words, take, upperFirst, toLower } from 'lodash-es'
+import { words, take, upperFirst, toLower, isEmpty } from 'lodash-es'
 
 export const initials = (value, maxInitials = 2) => {
   const wordValues = take(words(value), maxInitials)
@@ -11,4 +11,23 @@ export const initials = (value, maxInitials = 2) => {
 
 export const titleCase = (value) => {
   return upperFirst(toLower(value))
+}
+
+export const formatCurrency = (amount, currencyCode, maximumFractionDigits = 2, minimumFractionDigits = 0) => {
+  const locale = isEmpty(navigator.language) ? 'en' : navigator.language
+
+  let formatted
+
+  try {
+    formatted = new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: currencyCode,
+      maximumFractionDigits,
+      minimumFractionDigits,
+    }).format(amount)
+  } catch (e) {
+    formatted = amount
+  }
+
+  return formatted
 }
