4333eb85eac5c792dbf0c7993e14757a1f25a444
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-12-03 22:54:39 +0000

    missing

--- resources/js/Pages/Use/Index.vue
+++ resources/js/Pages/Use/Index.vue
@@ -9,7 +9,7 @@
       <div class="lg:min-w-0 lg:flex-1">
         <div class="pl-4 pr-6 pt-4 pb-4 border-b border-t border-gray-200 sm:pl-6 lg:sticky lg:bg-primary-yellow-50 lg:top-16 lg:z-10 lg:pl-8 xl:pl-6 xl:pt-6 xl:border-t-0">
           <div class="flex items-center">
-            <h1 class="flex-1 text-3xl leading-7 font-bold">Proposals</h1>
+            <h1 class="flex-1 text-3xl leading-7 font-bold">Posits</h1>
           </div>
         </div>
         <ul class="relative z-0 divide-y divide-gray-200 border-b border-gray-200 bg-white">
