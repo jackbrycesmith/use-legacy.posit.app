10f88de3379f34858f4170566d51748a900c2622
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-12-03 19:35:31 +0000

    translucent blur frosted glass go back home effect

--- resources/js/Components/ProposalBackHome.vue
+++ resources/js/Components/ProposalBackHome.vue
@@ -1,5 +1,8 @@
 <template>
-  <inertia-link :href="$route('use.index')" class="w-10 h-10 bg-primary-yellow-200 rounded-full flex items-center justify-center hover:bg-primary-yellow-100 shadow-md">
+  <inertia-link
+    :href="$route('use.index')"
+    class="w-10 h-10 rounded-full flex items-center justify-center shadow-md"
+    style="background-color: rgba(255, 255, 255, 0.5); backdrop-filter: blur(2px);">
     <ApplicationMark class="w-6 h-6" />
   </inertia-link>
 </template>
