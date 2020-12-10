e2943a3ed162c68112acfe28bc99f7d99be6a1d6
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-12-10 19:39:31 +0000

    attempt this for #202

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -14,6 +14,7 @@ class TrustProxies extends Middleware
      * @var array|string|null
      */
     protected $proxies = [
+        '10.0.0.0/0',
         '10.0.0.0',
         '10.0.0.1',
         '10.0.0.2',
