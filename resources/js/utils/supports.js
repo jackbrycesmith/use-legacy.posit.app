17129beaf335525bd455eabac5d30044443b9db7
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-12-11 19:51:15 +0000

    firefox video recording not working fixes #162

--- resources/js/utils/supports.js
+++ resources/js/utils/supports.js
@@ -5,6 +5,7 @@ export const getRecorderMimeType = () => {
 
   return [
     "video/webm; codecs=vp9",
+    "video/webm; codecs=vp8,opus",
     "video/webm; codecs=vp8",
     "video/webm; codecs=h264",
     "video/mp4; codecs=h264",
