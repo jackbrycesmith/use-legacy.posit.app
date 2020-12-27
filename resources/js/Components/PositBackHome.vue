90c3c46f10680f791a41ca920f0ddbd77e064e86
Jack Bryce-Smith <13235268+jackbrycesmith@users.noreply.github.com>
2020-12-27 00:39:07 +0000

    Make posit back home button nicer on focus

--- resources/js/Components/PositBackHome.vue
+++ resources/js/Components/PositBackHome.vue
@@ -1,7 +1,7 @@
 <template>
   <inertia-link
     :href="$route('use.index')"
-    class="w-10 h-10 rounded-full flex items-center justify-center shadow-md"
+    class="w-10 h-10 rounded-full flex items-center justify-center shadow-md focus:outline-none focus:shadow-lg hover:shadow-lg focus:text-primary-yellow-600 hover:text-primary-yellow-600 transition duration-200"
     style="background-color: rgba(255, 255, 255, 0.5); backdrop-filter: blur(2px);">
     <ApplicationMark class="w-6 h-6" />
   </inertia-link>
